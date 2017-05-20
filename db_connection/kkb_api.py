541d33582da9b7ca7b38d922951b6e2d66cd794e
bertanesen <bertan@useinsider.com>
2017-05-20 14:48:59 +0300

    set&getFunctions

--- db_connection/kkb_api.py
+++ db_connection/kkb_api.py
@@ -5,15 +5,43 @@ from kkb_data import DatabaseHelper
 app = FlaskAPI(__name__)
 
 
-@app.route('/kkb/data', methods=['POST'])
+@app.route('/kkb/set_data', methods=['POST'])
 def kkb_set_api():
     if request.method == 'POST':
         content = request.get_json()
-        return content
         udid = content["udid"]
+        print udid
         pid = content["product_id"]
+        print pid
         if udid is not None and pid is not None:
             DatabaseHelper().kkb_setter(udid,pid)
+        return content
+
+@app.route('/kkb/get_data', methods=['POST'])
+def kkb_get_api():
+    if request.method == 'POST':
+        content = request.get_json()
+        udid = content["udid"]
+        print udid
+        pid = content["product_id"]
+        print pid
+        if udid is not None and pid is not None:
+            DatabaseHelper().kkb_getter(udid,pid)
+        return content
+
+@app.route('/kkb/update_data', methods=['POST'])
+def kkb_update_api():
+    if request.method == 'POST':
+        content = request.get_json()
+        udid = content["udid"]
+        print udid
+        pid = content["product_id"]
+        print pid
+        duration = content["session_duration"]
+        print duration
+        if udid is not None and pid is not None and duration is not None:
+            DatabaseHelper().kkb_update(udid, pid, duration)
+        return content
 
 
 
--- db_connection/kkb_data.py
+++ db_connection/kkb_data.py
@@ -1,5 +1,5 @@
 import MySQLdb
-
+import datetime
 class DatabaseHelper(object):
 
 
@@ -29,6 +29,34 @@ class DatabaseHelper(object):
         except RuntimeError:
             print "Error"
 
+    def kkb_getter(self, udid, product_id):
+        try:
+            sql = "SELECT "
+            db = self.db_config("mydb")
+            print sql
+            cursor = db.cursor()
+            cursor.execute(sql)
+            db.commit()
+        except RuntimeError:
+            print "Error"
+
+    def kkb_update(self,  udid, product_id,session_duration):
+        try:
+            time =datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+            sql = "UPDATE `kkb_data` SET `session_duration` = {session_d}, is_active = 0, updated_date = '{updated}' " \
+                  "where udid= '{udid}' and " \
+                  "product_id = '{product_id}'".format(session_d = session_duration,
+                                                     updated = time,
+                                                     udid = udid,
+                                                     product_id = product_id)
+            print sql
+            db = self.db_config("mydb")
+            cursor = db.cursor()
+            cursor.execute(sql)
+            db.commit()
+        except RuntimeError:
+            print "Error"
 
 
 
