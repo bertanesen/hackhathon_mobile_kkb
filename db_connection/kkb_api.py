8564458980622435934efe4dc5b5834674671acf
bertanesen <bertan@useinsider.com>
2017-05-21 05:19:43 +0300

    frontEnd

--- db_connection/kkb_api.py
+++ db_connection/kkb_api.py
@@ -2,8 +2,11 @@ from modules import *
 from flask import request, url_for
 from flask.ext.api import FlaskAPI, status, exceptions
 from kkb_data import DatabaseHelper
+from flask.ext.cors import CORS, cross_origin
 
 app = FlaskAPI(__name__)
+app.config['CORS_HEADERS'] = 'Content-Type'
+cors = CORS(app, resources={r"/kkb/get_data": {"origins": "*"}})
 
 
 @app.route('/kkb/set_data', methods=['POST'])
@@ -14,9 +17,10 @@ def kkb_set_api():
         pid = content["product_id"]
         if udid is not None and pid is not None:
             DatabaseHelper().kkb_setter(udid,pid)
-        return content
+            return json.dumps({"count":"0"})
 
 @app.route('/kkb/get_data', methods=['POST'])
+@cross_origin(origin='localhost',headers=['Content- Type','Authorization'])
 def kkb_get_api():
     if request.method == 'POST':
         content = request.get_json()
@@ -24,6 +28,7 @@ def kkb_get_api():
         show_type = content["show_type"]
         if pid is not None and show_type is not None:
             result = DatabaseHelper().kkb_getter(pid, show_type)
+            print str(result)
             return json.dumps({"count": str(result)})
 
 
